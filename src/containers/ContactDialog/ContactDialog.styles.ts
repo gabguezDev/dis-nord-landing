670d9ef2243ab5d9a8a57e169e7deb6a5d3580ea
Gabriel <gabriel.dominguez@nextgo.io>
2022-10-27 20:37:39 -0300

    contact form added

---
+++ src/containers/ContactDialog/ContactDialog.styles.ts
@@ -0,0 +1,10 @@
+export const styles = {
+	innerDialogContainer: { m: 3 },
+	dialogTitle: { flex: 1, fontFamily: "Nunito", color: "#000" },
+	contactFormWrapper: {
+		height: "100%",
+		display: "flex",
+		flexDirection: "column",
+		justifyContent: "center",
+	},
+};
